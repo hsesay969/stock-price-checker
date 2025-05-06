99f5b874fac7891a1496097b4228b2bf89b9e2a7
hsesay969 <henrys20251@gmail.com>
2025-05-06 14:12:51 +0300

    Configured Content Security Policy

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
 'use strict';
 require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet'); // Import Helmet
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const cors        = require('cors');
+const helmet      = require('helmet');
 
-const apiRoutes = require('./routes/api.js');
-const fccTestingRoutes = require('./routes/fcctesting.js');
-const runner = require('./test-runner');
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
 
 require("./db-connection");
 
 const app = express();
 
-// Use Helmet for security headers
+// Security middleware
 app.use(helmet());
-
-// Configure Content Security Policy using Helmet's middleware
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'"],
-      styleSrc: ["'self'"],
-      // Add other directives as needed, but for this requirement, 'self' for scripts and styles is key.
-    },
-  })
-);
+// Set Content Security Policy
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'"],
+    styleSrc: ["'self'"]
+  }
+}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({ origin: '*' })); //For FCC testing purposes only
+app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -44,11 +40,11 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API
-apiRoutes(app);
-
+//Routing for API 
+apiRoutes(app);  
+    
 //404 Not Found Middleware
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
@@ -57,12 +53,12 @@ app.use(function (req, res, next) {
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-  if (process.env.NODE_ENV === 'test') {
+  if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
-      } catch (e) {
+      } catch(e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
